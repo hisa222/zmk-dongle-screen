c23f8450fe6e76de5f341970c49144a969fbe528
hisa222 <h.suzuki@hakusui-sangyou.com>
2026-05-26 16:21:17 +0900

    Update touch_handler.h

--- boards/shields/dongle_screen/touch_handler.h
+++ boards/shields/dongle_screen/touch_handler.h
@@ -1,5 +1,9 @@
 // touch_handler.h
 #pragma once
+#include <lvgl.h>
+
+// status_screen.c で定義、touch_handler.c から参照する
+extern lv_obj_t *dongle_main_screen;
 
 #ifdef CONFIG_DONGLE_SCREEN_TOUCH_ENABLED
 void touch_handler_init(void);
