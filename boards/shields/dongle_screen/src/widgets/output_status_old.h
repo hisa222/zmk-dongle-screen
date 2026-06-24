8c41acc87c0ed92ed90be00f80d0d7d6a6ddbcf6
hisa222 <h.suzuki@hakusui-sangyou.com>
2026-06-24 14:58:54 +0900

    Update output_status_old.h

--- boards/shields/dongle_screen/src/widgets/output_status_old.h
+++ boards/shields/dongle_screen/src/widgets/output_status_old.h
@@ -10,7 +10,7 @@
 #include <zephyr/kernel.h>
 
 // output_status.h
-struct zmk_widget_output_status
+struct zmk_widget_output_status_old
 {
     lv_obj_t *obj;
     lv_obj_t *transport_label;
@@ -18,5 +18,5 @@ struct zmk_widget_output_status
     sys_snode_t node;
 };
 
-int zmk_widget_output_status_init(struct zmk_widget_output_status *widget, lv_obj_t *parent);
-lv_obj_t *zmk_widget_output_status_obj(struct zmk_widget_output_status *widget);
+int zmk_widget_output_status_init(struct zmk_widget_output_status_old *widget, lv_obj_t *parent);
+lv_obj_t *zmk_widget_output_status_obj(struct zmk_widget_output_status_old *widget);
