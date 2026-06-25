9b6c0e6440ac87cefa7dcef22e7d3c1e5a4ac633
hisa222 <h.suzuki@hakusui-sangyou.com>
2026-06-25 17:16:39 +0900

    Update brightness_screen.h

--- boards/shields/dongle_screen/src/widgets/brightness_screen.h
+++ boards/shields/dongle_screen/src/widgets/brightness_screen.h
@@ -8,18 +8,24 @@
 #include <lvgl.h>
 #include <stdbool.h>
 
+/*
+ * brightness_screen が現在使うスライダー操作状態。
+ * touch_handler.c の weak 関数 display_settings_is_interacting() を
+ * brightness_screen.c でオーバーライドして参照する。
+ */
 struct zmk_widget_brightness_screen {
-    lv_obj_t *obj;             /* root container */
+    lv_obj_t *obj;          /* = parent そのもの（コンテナなし）*/
     lv_obj_t *title_label;
-    lv_obj_t *value_label;
     lv_obj_t *slider;
-    lv_obj_t *hint_label;
-
-    uint8_t current_value;
-    bool dragging;
+    lv_obj_t *value_label;
+    lv_obj_t *icon_low;
+    lv_obj_t *icon_high;
+    lv_obj_t *nav_hint;
 };
 
 int  zmk_widget_brightness_screen_init(struct zmk_widget_brightness_screen *widget,
                                        lv_obj_t *parent);
+
+/* スクリーン表示/非表示時に呼ぶ。スライダー値同期とフラグ管理を行う。 */
 void zmk_widget_brightness_screen_show(struct zmk_widget_brightness_screen *widget);
 void zmk_widget_brightness_screen_hide(struct zmk_widget_brightness_screen *widget);
