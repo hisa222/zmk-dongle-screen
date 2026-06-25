4fab4232130adf902a6e5b3d2d92dc89ddf049bf
hisa222 <h.suzuki@hakusui-sangyou.com>
2026-06-25 09:12:33 +0900

    Update touch_handler.h

--- boards/shields/dongle_screen/src/touch_handler.h
+++ boards/shields/dongle_screen/src/touch_handler.h
@@ -41,12 +41,12 @@ int touch_handler_register_callback(touch_event_callback_t callback);
 int touch_handler_get_last_event(struct touch_event_data *event);
 
 /**
- * register LVGL indev
+ * register LVGL indev (LVGL8: lv_indev_drv_init + lv_indev_drv_register)
  */
 int touch_handler_register_lvgl_indev(void);
 
 /**
- * true while a touch/swipe gesture is in progress
+ * true while a touch/swipe gesture is in progress (swipe_state.in_progress)
  */
 bool touch_handler_is_swiping(void);
 
@@ -55,5 +55,9 @@ bool touch_handler_is_swiping(void);
  *
  * brightness slider や system settings ボタン操作中は true にする。
  * custom_status_screen.c 側はこれを見てスワイプ遷移を抑止する。
+ *
+ * brightness_screen.c: PRESSED で true、RELEASED/PRESS_LOST で false
+ *                      縦スワイプキャンセル時は即 false (スワイプ通過のため)
+ * system_settings_widget.c: ボタン PRESSED で true、RELEASED/PRESS_LOST で false
  */
 extern volatile bool ui_interaction_active;
