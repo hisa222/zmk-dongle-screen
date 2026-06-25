87cd138fe0baa76c7ee9d25c19a25755b46bef82
hisa222 <h.suzuki@hakusui-sangyou.com>
2026-06-25 09:53:00 +0900

    Create brightness_screen.h

---
+++ boards/shields/dongle_screen/src/widgets/brightness_screen.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <lvgl.h>
+#include <stdbool.h>
+
+/*
+ * custom_status_screen.c 側で定義される共有フラグ。
+ * スライダ操作中は true にする。
+ */
+extern bool ui_interaction_active;
+
+struct zmk_widget_brightness_screen {
+    lv_obj_t *obj;             /* root container */
+    lv_obj_t *title_label;
+    lv_obj_t *value_label;
+    lv_obj_t *slider;
+    lv_obj_t *hint_label;
+
+    uint8_t current_value;
+    bool dragging;
+};
+
+int  zmk_widget_brightness_screen_init(struct zmk_widget_brightness_screen *widget,
+                                       lv_obj_t *parent);
+void zmk_widget_brightness_screen_show(struct zmk_widget_brightness_screen *widget);
+void zmk_widget_brightness_screen_hide(struct zmk_widget_brightness_screen *widget);
