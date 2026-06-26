f09218df10e76c6fc36636369439a7be3079315d
hisa222 <h.suzuki@hakusui-sangyou.com>
2026-06-26 17:17:53 +0900

    Add files via upload

---
+++ boards/shields/dongle_screen/src/widgets/keyboard_name_status.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#include <zmk/display.h>
+#include <zmk/display/widgets/keyboard_name_status.h>
+
+int zmk_widget_keyboard_name_status_init(struct zmk_widget_keyboard_name_status *widget,
+                                         lv_obj_t *parent)
+{
+    widget->obj = lv_label_create(parent);
+
+    /* CONFIG_ZMK_KEYBOARD_NAME で定義されたキーボード名を表示 */
+    lv_label_set_text(widget->obj, CONFIG_ZMK_KEYBOARD_NAME);
+
+    /* フォント設定 */
+    lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_16, 0);
+
+    /* 文字色を青色に設定 */
+    lv_obj_set_style_text_color(widget->obj, lv_color_hex(0x0000FF), 0);
+
+    return 0;
+}
+
+lv_obj_t *zmk_widget_keyboard_name_status_obj(struct zmk_widget_keyboard_name_status *widget)
+{
+    return widget->obj;
+}
