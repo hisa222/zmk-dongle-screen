162535833f153e5cc87120d5be343b697d35ace9
hisa222 <h.suzuki@hakusui-sangyou.com>
2026-07-02 10:36:35 +0900

    Create media_control_widget.h

---
+++ boards/shields/dongle_screen/src/widgets/media_control_widget.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <lvgl.h>
+#include <stdbool.h>
+
+struct zmk_widget_media_control {
+    lv_obj_t *obj;
+    lv_obj_t *title_label;
+    lv_obj_t *mute_btn;
+    lv_obj_t *vol_down_btn;
+    lv_obj_t *vol_up_btn;
+    lv_obj_t *nav_hint;
+};
+
+int zmk_widget_media_control_init(struct zmk_widget_media_control *widget,
+                                   lv_obj_t *parent);
