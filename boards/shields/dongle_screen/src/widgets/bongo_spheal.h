b34226751edfc2955d13d4086c2719e01cc45a44
hisa222 <h.suzuki@hakusui-sangyou.com>
2026-07-03 16:54:27 +0900

    Update bongo_spheal.h

--- boards/shields/dongle_screen/src/widgets/bongo_spheal.h
+++ boards/shields/dongle_screen/src/widgets/bongo_spheal.h
@@ -9,10 +9,10 @@
 #include <lvgl.h>
 #include <zephyr/kernel.h>
 
-struct zmk_widget_bongo_boo {
+struct zmk_widget_bongo_spheal {
     sys_snode_t node;
     lv_obj_t *obj;
 };
 
-int zmk_widget_bongo_boo_init(struct zmk_widget_bongo_boo *widget, lv_obj_t *parent);
-lv_obj_t *zmk_widget_bongo_boo_obj(struct zmk_widget_bongo_boo *widget);
+int zmk_widget_bongo_spheal_init(struct zmk_widget_bongo_spheal *widget, lv_obj_t *parent);
+lv_obj_t *zmk_widget_bongo_spheal_obj(struct zmk_widget_bongo_spheal *widget);
