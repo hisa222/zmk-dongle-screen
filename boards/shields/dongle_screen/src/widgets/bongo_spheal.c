04407e8079135d6f565c1d156ea1bb82755b58fa
hisa222 <h.suzuki@hakusui-sangyou.com>
2026-07-06 13:35:46 +0900

    Update bongo_spheal.c

--- boards/shields/dongle_screen/src/widgets/bongo_spheal.c
+++ boards/shields/dongle_screen/src/widgets/bongo_spheal.c
@@ -163,11 +163,11 @@ widget->obj = lv_animimg_create(parent);
 lv_animimg_set_src(widget->obj, SRC(idle_imgs_s));
 // lv_img_set_zoom(widget->obj, 768);
 lv_img_set_zoom(widget->obj, 384);
-lv_obj_set_size(widget->obj, 90 * 1, 47 * 1);
+lv_obj_set_size(widget->obj, 90 * 1.5, 47 * 1.5);
 lv_obj_center(widget->obj);
 
-lv_obj_set_style_bg_color(widget->obj, lv_color_hex(0xFF0000), 0);
-lv_obj_set_style_bg_opa(widget->obj, LV_OPA_COVER, 0);
+// lv_obj_set_style_bg_color(widget->obj, lv_color_hex(0xFF0000), 0);
+// lv_obj_set_style_bg_opa(widget->obj, LV_OPA_COVER, 0);
     
 lv_animimg_set_duration(widget->obj, ANIMATION_SPEED_IDLE);
 lv_animimg_set_repeat_count(widget->obj, LV_ANIM_REPEAT_INFINITE);
