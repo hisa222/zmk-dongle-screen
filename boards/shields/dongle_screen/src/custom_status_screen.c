c9d3cceda4ef9a468654aab41bcc7b3398f15ec8
hisa222 <h.suzuki@hakusui-sangyou.com>
2026-07-06 14:32:30 +0900

    Update custom_status_screen.c

--- boards/shields/dongle_screen/src/custom_status_screen.c
+++ boards/shields/dongle_screen/src/custom_status_screen.c
@@ -205,7 +205,7 @@ static lv_obj_t *create_main_screen(void)
     zmk_widget_bongo_spheal_init(&main_bongo_spheal_widget, screen);
     lv_obj_align(zmk_widget_bongo_spheal_obj(&main_bongo_spheal_widget),
 //                 LV_ALIGN_BOTTOM_MID, 0, 0);
-                LV_ALIGN_CENTER, -40, 20);
+                LV_ALIGN_CENTER, -20, 40);
 #endif
 
 #else //-------------------------------------------------------------------------------------------------------------------
