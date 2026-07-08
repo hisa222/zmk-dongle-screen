7851f6fda9fd687d2cfd2b396d4347f1a21e4d34
hisa222 <h.suzuki@hakusui-sangyou.com>
2026-07-08 10:00:45 +0900

    Update custom_buttons.h

--- boards/shields/dongle_screen/src/widgets/custom_buttons.h
+++ boards/shields/dongle_screen/src/widgets/custom_buttons.h
@@ -2,23 +2,26 @@
  * Copyright (c) 2024 The ZMK Contributors
  * SPDX-License-Identifier: MIT
  */
-
 #pragma once
-
 #include <lvgl.h>
 #include <stdbool.h>
 
-struct zmk_custom_buttons {
+struct zmk_widget_custom_buttons {
     lv_obj_t *obj;
     lv_obj_t *title_label;
-    lv_obj_t *custom_button_1;
-    lv_obj_t *custom_button_2;
-    lv_obj_t *custom_button_3;
-    lv_obj_t *custom_button_4;
-    lv_obj_t *custom_button_5;
-    lv_obj_t *custom_button_6;
+    lv_obj_t *custom_button_1_btn;
+    lv_obj_t *custom_button_2_btn;
+    lv_obj_t *custom_button_3_btn;
+    lv_obj_t *custom_button_4_btn;
+    lv_obj_t *custom_button_5_btn;
+    lv_obj_t *custom_button_6_btn;
     lv_obj_t *nav_hint;
 };
 
 int zmk_widget_custom_buttons_init(struct zmk_widget_custom_buttons *widget,
                                    lv_obj_t *parent);
+
+static inline lv_obj_t *zmk_widget_custom_buttons_obj(struct zmk_widget_custom_buttons *widget)
+{
+    return widget->obj;
+}
